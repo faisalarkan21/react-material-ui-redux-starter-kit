6818a68df0807a0d0bec0050946148165a4fcb61
faisal arkan <faisalarkan21@gmail.com>
2017-11-05 18:49:22 +0700

    choose using scss over css

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -41,6 +41,17 @@ const config = {
           },
         ],
 
+      }, {
+
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader', // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader', // compiles Sass to CSS
+        }],
+
       },
     ],
   },
