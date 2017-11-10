c795a0e74e3464187f0a3ca1add9efaa37209c84
faisal arkan <faisalarkan21@gmail.com>
2017-11-10 19:22:28 +0700

    add redux form component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
-
+import { reducer as formReducer } from 'redux-form';
 import { reducerText, reducerNumbers, reducerStrictText } from './validator';
 
 const allReducers = combineReducers({
   errorMessageText: reducerText,
   errorMessageNumbers: reducerNumbers,
   strictText: reducerStrictText,
+  form: formReducer,
 });
 
 export default allReducers;
