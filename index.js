c2b45b0bd524d67ff18cfebab2606f2a0fac201d
faisal arkan <faisalarkan21@gmail.com>
2017-11-10 19:22:52 +0700

    configure react router

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+
 import store from './src/store/configureStore';
-import App from './src/containers/App';
+import custom from './src/containers/custom-components';
+import reduxForm from './src/containers/redux-form-components';
 // eslint-disable-next-line
 import 'typeface-roboto'; 
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={custom} />
+        <Route path="/about" component={reduxForm} />
+
+      </div>
+    </Router>
   </Provider>
   , document.getElementById('root'),
 );
