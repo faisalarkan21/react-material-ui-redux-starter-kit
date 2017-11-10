8b4d6ac81eb451f16e3d0dfc2d28839173763399
faisal arkan <faisalarkan21@gmail.com>
2017-11-10 13:58:35 +0700

    rename recuder strict text

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 
-import { reducerText, reducerNumbers } from './validator';
+import { reducerText, reducerNumbers, reducerStrictText } from './validator';
 
 const allReducers = combineReducers({
   errorMessageText: reducerText,
   errorMessageNumbers: reducerNumbers,
+  strictText: reducerStrictText,
 });
 
 export default allReducers;
