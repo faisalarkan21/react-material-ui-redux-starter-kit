82af39e9355e4055fb9d3a0b106dd7da3776ccd1
faisal arkan <faisalarkan21@gmail.com>
2017-11-10 13:56:52 +0700

    refactor code formatCustom.js

--- src/middleware/FormatCustom.js
+++ src/middleware/FormatCustom.js
@@ -2,31 +2,35 @@ import React from 'react';
 import NumberFormat from 'react-number-format';
 import MaskedInput from 'react-text-mask';
 
-
-function NumberFormatRp(props) {
-  return (
-    <NumberFormat
-      {...props}
-      thousandSeparator="."
-      decimalScale={2}
-      decimalSeparator=","
-      fixedDecimalScale
-      prefix="Rp. "
-    />
-  );
+class NumberFormatRp extends React.Component {
+  render() {
+    return (
+      <NumberFormat
+        {...this.props}
+        thousandSeparator="."
+        decimalScale={2}
+        decimalSeparator=","
+        fixedDecimalScale
+        prefix="Rp. "
+      />
+    );
+  }
 }
 
-function TextMaskCustomPhone(props) {
-  return (
-    <MaskedInput
-      {...props}
-      mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+
+class TextMaskCustomPhone extends React.Component {
+  render() {
+    return (
+      <MaskedInput
+        {...this.props}
+        mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
 
       // Unicode should using curly braces.
       // eslint-disable-next-line
-      placeholderChar={'\u2000'}           
-    />
-  );
+      placeholderChar={'\u2000'}
+      />
+    );
+  }
 }
 
 export { NumberFormatRp, TextMaskCustomPhone };
