b0824330d5cc1fcccfc0644e67e433fabad439ee
faisal arkan <faisalarkan21@gmail.com>
2017-11-11 10:47:52 +0700

    add eslint rules specialLink [react router]

--- src/components/index.js
+++ src/components/index.js
@@ -17,7 +17,7 @@ class Dashboard extends Component {
           <ColMod xs={12} md={8} lg={9} lgOffset={1} >
             <PaperMod elevation={19}>
               <Row >
-                <DisplayMod1 text="React Redux Material UI">
+                <DisplayMod1 text="React Material UI">
                   <CodeStyleMod text="Example" />
                 </DisplayMod1>
               </Row>
