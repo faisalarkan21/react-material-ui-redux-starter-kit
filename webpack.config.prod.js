f8733b31a872d7b5ac4057d44fa5b4e447e349c5
faisal arkan <faisalarkan21@gmail.com>
2017-11-12 02:14:01 +0700

    add scss webpack production config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -47,6 +47,17 @@ const config = {
           },
         ],
 
+      }, {
+
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader', // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader', // compiles Sass to CSS
+        }],
+
       },
     ],
   },
