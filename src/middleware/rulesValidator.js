a9d776d415191f686219eb8396db00902cbb2acb
faisal arkan <faisalarkan21@gmail.com>
2017-11-13 00:36:20 +0700

    make parseMessage rules

--- src/middleware/rulesValidator.js
+++ src/middleware/rulesValidator.js
@@ -1,17 +1,24 @@
 const filterRegex = {
-  String: /^[a-zA-Z]+$/,
+  String: /^[a-zA-Z\s]+$/,
   Number: /^[0-9\b]+$/,
 };
 
 
 const customMessage = {
-  statusStringError: 'Input only characters..',
-  statusNumError: 'Input only numbers..',
-  statusNull: 'Input is required..',
+  stringError: 'Input only letter..',
+  numberError: 'Input only numbers..',
+  nullError: 'Input is required..',
+
 };
 
+function parseMessage(key = '', err = false) {
+  return Object.assign({
+    message: customMessage[key],
+    isError: err,
+  });
+}
 
-module.exports = {
-  customMessage, filterRegex,
+export {
+  customMessage, filterRegex, parseMessage,
 };
 
